c3443e665a87053ebc36c1e2e4a87f1cb531167a
JasonZhangX <v-zhlong@microsoft.com>
2018-09-23 03:07:34 +0800

    Adapt to different screen resolutions

--- index.js
+++ index.js
@@ -3,48 +3,97 @@ var robot = require("robotjs");
 var Jimp = require('jimp');
 var screenshot = require('screenshot-desktop');
 var findImagePos = require('./util/findImage');
+var schedule = require('node-schedule');
+var config = require('./data/config.json');
+var task = require('./data/task.json');
+var schedule = require('node-schedule');
 
 // Speed up the mouse.
 robot.setMouseDelay(500);
 
-var screenSize = robot.getScreenSize();
-var screenHeight = screenSize.height;
-var screenWidth = screenSize.width;
+var doAction = function(actionName) {
+    var screenSize = robot.getScreenSize();
+    var screenHeight = screenSize.height;
+    var screenWidth = screenSize.width;
+    var moveRatio = 0.8;
+    console.log('Screen Width:' + screenWidth);
+    console.log('Screen Height:' + screenHeight);
+    //real screen width：819
+    //screenshot width: 1024
+    //ratio: 819/1024 = 0.7998 = 0.8
+    screenshot({
+        filename: config.fullScreenshotUrl
+    }).then((imgPath) => {
+        console.log(imgPath);
+        Jimp.read(config.fullScreenshotUrl).then(image => {
+            moveRatio = screenWidth / image.getWidth();
+            findImagePos(config.fullScreenshotUrl, config.targetUrl, function(skypeBasePosition) {
+                //Move the mouse to the position.
+                var statusPosition = {
+                    x: skypeBasePosition.posX + config.statusPositionOffset.x,
+                    y: skypeBasePosition.posY + config.statusPositionOffset.y
+                }
+                var statusAvailablePosition = {
+                    x: statusPosition.x + config.statusAvailablePositionOffset.x,
+                    y: statusPosition.y + config.statusAvailablePositionOffset.y
+                }
 
-//
-screenshot({ filename: 'screenshot/fullScreenshot.png' });
+                var statusAwayPosition = {
+                    x: statusPosition.x + config.statusAwayPositionOffset.x,
+                    y: statusPosition.y + config.statusAwayPositionOffset.y
+                }
 
-//
-findImagePos('./screenshot/fullScreenshot.png', './screenshot/target.png', function(skypeBasePosition) {
-    //Move the mouse to the position.
-    var statusPosition = {
-        x: skypeBasePosition.posX + 88,
-        y: skypeBasePosition.posY + 85
-    }
-    var statusAvailablePosition = {
-        x: statusPosition.x,
-        y: statusPosition.y + 20
-    }
+                //Get skype window focus
+                robot.moveMouse(skypeBasePosition.posX * moveRatio, skypeBasePosition.posY * moveRatio);
+                robot.mouseClick();
+                screenshot({
+                    filename: 'screenshot/window-active.png'
+                });
+                switch (actionName) {
+                    case 'online':
 
-    var statusAwayPosition = {
-        x: statusPosition.x,
-        y: statusPosition.y + 150
-    }
+                        //Move to the status list menu
+                        robot.moveMouse(statusPosition.x * moveRatio, statusPosition.y * moveRatio);
+                        robot.mouseClick();
+                        screenshot({
+                            filename: 'screenshot/status-menu.png'
+                        });
+                        //Change the status to available
+                        robot.moveMouse(statusAvailablePosition.x * moveRatio, statusAvailablePosition.y * moveRatio);
+                        robot.mouseClick();
+                        screenshot({
+                            filename: 'screenshot/status-online.png'
+                        });
+                        break;
 
-    //Get skype window focus
-    robot.moveMouse(skypeBasePosition.posX, skypeBasePosition.posY);
-    robot.mouseClick();
-    //Move to the status list menu
-    robot.moveMouse(statusPosition.x, statusPosition.y);
-    robot.mouseClick();
-    //Change the status to available
-    robot.moveMouse(statusAvailablePosition.x, statusAvailablePosition.y);
-    robot.mouseClick();
-    //Move to the status list menu
-    robot.moveMouse(statusPosition.x, statusPosition.y);
-    robot.mouseClick();
-    //Change the status to away
-    robot.moveMouse(statusAwayPosition.x, statusAwayPosition.y);
-    robot.mouseClick();
+                    case 'away':
+                        //Move to the status list menu
+                        robot.moveMouse(statusPosition.x * moveRatio, statusPosition.y * moveRatio);
+                        robot.mouseClick();
 
-});
+                        //Change the status to away
+                        robot.moveMouse(statusAwayPosition.x * moveRatio, statusAwayPosition.y * moveRatio);
+                        robot.mouseClick();
+                        break;
+                }
+
+                //Lose the skype window focus
+                robot.moveMouse(0, 0);
+                robot.mouseClick();
+
+            });
+        });
+    });
+
+
+}
+
+for (var i = 0; i < task.length; i++) {
+    var taskItem = task[i];
+    taskItem.schedule = (function(taskItem) {
+        return schedule.scheduleJob(taskItem.time, function() {
+            doAction(taskItem.action);
+            console.log('The task ' + taskItem.taskName + 'is done!');
+        });
+    })(taskItem);
+}
